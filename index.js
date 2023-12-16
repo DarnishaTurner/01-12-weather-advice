b408d64ddf0fab89002e9165bd944b0c2304a586
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-16 10:06:12 -0500

    Darnisha finished work

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const weatherConditions = ['sunny', 'rainy', 'cloudy', 'windy'];
 // Suggest activities based on weather conditions using if-else statements
 weatherConditions.forEach(condition => {
   let activity = '';
-
-  // Write your if-else statements here for specific suggestions
+if (condition === `sunny`) {
+  activity = `Go for a walk`;
+} else if( condition === `rainy`){
+  activity = `Bring an umbrella`;
+} else if( condition === `cloudy`) {
+  activity = `Great day for a museum visit`;
+} else if (condition === `windy`) {
+  activity = `Perfect for flying kites`;
+}
 
   console.log(`When it's ${condition}, you should ${activity}.`);
 });
